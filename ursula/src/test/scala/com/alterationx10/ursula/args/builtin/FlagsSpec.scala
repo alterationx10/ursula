fc4461801a5ac5bd92af3f73ebbf1f5eedc9b574
Mark Rudolph <mark@k8ty.app>
2022-05-28 21:03:22 -0400

    adding in some FlagsSpec tests

--- ursula/src/test/scala/com/alterationx10/ursula/args/builtin/FlagsSpec.scala
+++ ursula/src/test/scala/com/alterationx10/ursula/args/builtin/FlagsSpec.scala
@@ -1,16 +1,50 @@
 package com.alterationx10.ursula.args.builtin
 
-import zio.test.{Spec, TestEnvironment, ZIOSpecDefault, assertCompletes}
-import zio.{Scope, ZIO}
+import zio.test._
+import zio.test.Assertion._
+import zio._
 
 object FlagsSpec extends ZIOSpecDefault {
 
+  import Flags._
+
+  def toChunk(str: String): Chunk[String] =
+    Chunk.fromArray(str.split(" "))
+
+  val commandArgs: Seq[Chunk[String]] = Seq(
+    "-h",
+    "--help",
+    "command -h",
+    "command --help",
+    "command h help arg1 arg2"
+  ).map(toChunk)
+
   override def spec: Spec[TestEnvironment with Scope, Any] =
     suite("FlagsSpec")(
-      test("")(
-        for {
-          _ <- ZIO.fail("TestNotImplemented")
-        } yield assertCompletes
+      suite("helpFlag")(
+        test("name")(
+          assert(helpFlag.name)(equalTo("help"))
+        ),
+        test("shortKey")(
+          assert(helpFlag.shortKey)(equalTo("h"))
+        ),
+        test("isBoolean")(
+          assert(helpFlag.expectsArgument)(isFalse)
+        ),
+        test("no conflicts")(
+          assert(helpFlag.exclusive)(isNone)
+        ),
+        test("no dependencies")(
+          assert(helpFlag.dependsOn)(isNone)
+        ),
+        test("isPresent")(
+          for {
+            r <- ZIO.foreach(commandArgs)(helpFlag.isPresentZIO)
+          } yield assertTrue(
+            r.count(_ == true) == 4,
+            r.count(_ == false) == 1
+          )
+        )
       )
     )
 
