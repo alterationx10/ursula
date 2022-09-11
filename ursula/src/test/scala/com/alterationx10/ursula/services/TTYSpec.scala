7c692c330247ed937d241ef9e1d146939441018f
Mark Rudolph <mark@k8ty.app>
2022-09-11 00:02:08 -0400

    add native friendly Console

---
+++ ursula/src/test/scala/com/alterationx10/ursula/services/TTYSpec.scala
@@ -0,0 +1,56 @@
+package com.alterationx10.ursula.services
+
+import com.alterationx10.ursula.extensions.UrsulaTestExtensions
+import utest.*
+import zio.*
+
+import java.io.{ByteArrayOutputStream, IOException, StringReader}
+
+object TTYSpec extends TestSuite with UrsulaTestExtensions {
+
+  implicit val rt: Runtime.Scoped[Config] =
+    ConfigLive.live.testRuntime
+
+  override def tests: Tests = Tests {
+    test("readLine") {
+
+      val result = scala.Console.withIn(new StringReader("abc")) {
+        TTY.readLine.testValue == "abc"
+      }
+      assert(result)
+    }
+
+    test("print") {
+      val out = new ByteArrayOutputStream()
+      scala.Console.withOut(out) {
+        TTY.print("abc").testValue
+      }
+      assert(out.toString == "abc")
+    }
+
+    test("printError") {
+      val out = new ByteArrayOutputStream()
+      scala.Console.withOut(out) {
+        TTY.printError("abc").testValue
+      }
+      assert(out.toString == "abc")
+    }
+
+    test("printLibe") {
+      val out = new ByteArrayOutputStream()
+      scala.Console.withOut(out) {
+        TTY.printLine("abc").testValue
+      }
+      assert(out.toString == "abc\n")
+    }
+
+    test("printLineError") {
+      val out = new ByteArrayOutputStream()
+      scala.Console.withOut(out) {
+        TTY.printLineError("abc").testValue
+      }
+      assert(out.toString == "abc\n")
+    }
+
+  }
+}
